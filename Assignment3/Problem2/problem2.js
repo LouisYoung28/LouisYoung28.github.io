adc2ed2704eeba1662fad3ae3f2449c1f8df980d
Louis Young <layoung2@aggies.ncat.edu>
2019-09-23 23:27:14 -0400

    Completed Part 2 of Assignment 3

---
+++ Assignment3/Problem2/problem2.js
@@ -0,0 +1,48 @@
+function go(){
+    var scores = [];
+    var index;
+    var numExams = prompt("Please enter number of exams taken: ");
+    scores = getScores(numExams);
+    index = maxExam(scores);
+    outScores(scores);
+}
+
+function getScores(numExams){
+    var scores = [];
+    for( i=0; i<numExams; i++)
+        scores[i] = prompt("Please enter the score for exam " + (i+1) + " below: ");
+    return scores;
+}
+
+function maxExam(scores){
+    var index;
+    for(var i in scores){
+        if(parseInt(i) == 0){
+            maxNum = scores[i];
+            index = i;
+        }
+        else if(scores[i] > maxNum){
+            maxNum = scores[i];
+            index = i;
+        }
+        else
+        i = parseInt(i) + 1;
+    }
+    document.getElementById("wr_span").innerText = "The highest score, " + scores[index] + ", was on Exam " + (parseInt(index)+1);
+    return maxNum;
+}
+
+function outScores(scores){
+    var output = "<table border = '2' ><tr>";
+    for(var i in scores){
+        output = output + "<td>Exam " + (parseInt(i)+1) + "</td>";
+        i = parseInt(i) + 1;
+    }
+    output = output + "</tr><tr>";
+    for(var i in scores){
+        output = output + "<td>" + scores[i] + "</td>";
+        i = parseInt(i) + 1;
+    }
+    output = output + "</tr></table>";
+    document.getElementById("wr_table").innerHTML = output;
+}
